94cf42bcdc7f56224e3391207577a7c2999fc192
Александр Иннокентьевич Чемезов <alexchemez@gmail.com>
2023-02-10 15:06:19 +0900

    Update block_nlrsbook_ilim.php

--- lang/en/block_nlrsbook_ilim.php
+++ lang/en/block_nlrsbook_ilim.php
@@ -3,5 +3,3 @@ $string['pluginname'] = 'Библиопоиск «Илим»';
 $string['nlrsbook_ilim'] = 'Библиопоиск «Илим»';
 $string['nlrsbook_ilim:addinstance'] = 'Add a new Библиопоиск «Илим» block';
 $string['nlrsbook_ilim:myaddinstance'] = 'Add a new Библиопоиск «Илим» to the My Moodle page';
-
-$string['org_token'] = 'Токен организации';
