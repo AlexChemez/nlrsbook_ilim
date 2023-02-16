213ac37987d2db40259315bdcaadb9489759900c
gradientascent100 <52383414+gradientascent100@users.noreply.github.com>
2023-02-16 20:37:48 +0900

    Удаление закомментированного кода в search.php

--- search.php
+++ search.php
@@ -1,7 +1,5 @@
 <?php
 
-// die('d');
-
 require(__DIR__ . '/../../config.php');
 
 require_once($CFG->dirroot . "/blocks/nlrsbook_auth/Query.php");
@@ -9,58 +7,8 @@ require_login();
 use App\Querys\Query;
 
 
-// require_once(__DIR__ . '/lib.php');
-
-// global $CFG;
-// global $DB;
-
-// die('zz');
-
-// $id = optional_param('id', 0, PARAM_INT);
-// $i = optional_param('i', 0, PARAM_INT);
-
-
-
-// if ($id) {
-    // $cm = get_coursemodule_from_id('nlrsbook', $id, 0, false, MUST_EXIST);
-//     $course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
-//     $moduleinstance = $DB->get_record('nlrsbook', array('id' => $cm->instance), '*', MUST_EXIST);
-// } else if ($i) {
-//     $moduleinstance = $DB->get_record('nlrsbook', array('id' => $n), '*', MUST_EXIST);
-//     $course = $DB->get_record('course', array('id' => $moduleinstance->course), '*', MUST_EXIST);
-//     $cm = get_coursemodule_from_instance('nlrsbook', $moduleinstance->id, $course->id, false, MUST_EXIST);
-// } else {
-//     print_error(get_string('missingidandcmid', 'mod_nlrsbook'));
-// }
-
-// require_login($course, true, $cm);
-
-// $host = 'https://e.nlrs.ru/graphql';
-// $user_id = $USER->id;
-// $instance = $DB->get_record('nlrsbook_shelf', array('user_id' => $USER->id), '*', IGNORE_MISSING );
-
-// $nlrsbook_id = $moduleinstance->nlrsbook_id;
-
-// if ($instance->token) {
-//     $token = $instance->token;
-// } else {
-//     $getToken = checkToken($user_id, $host);
-//     $row = new stdClass();
-//     $row->user_id = $user_id;
-//     $row->token = $getToken;
-//     $row->datetime = '1';
-//     $DB->insert_record('nlrsbook_shelf', $row);
-//     $token = $getToken;
-// }
-// $token = 'asdasd';
-
-
-// $modulecontext = context_module::instance();
-// $bookdata = getShelf($nlrsbook_id, $token);
-// $PAGE->set_url('/mod/nlrsbook/asd.php', array());
 $PAGE->set_title('Библиопоиск «Илим»');
 $PAGE->set_heading('Библиопоиск «Илим»');
-// $PAGE->set_context($modulecontext);
 $PAGE->set_pagelayout('standard');
 
 $searchUrl = $CFG->wwwroot.'/blocks/nlrsbook_ilim/search.php';
@@ -113,10 +61,6 @@ $template = <<<XML
     ecsbEpsEfed.renderSearchUI();
 </script>
 XML;
-
-// $template = str_replace('{{ $searchUrl }}', $CFG->wwwroot.'/blocks/nlrsbook_ilim/search.php', $template);
-// $template = str_replace('{{ $online2Url }}', $online2Url, $template);
-
 
 echo $OUTPUT->header();
 
